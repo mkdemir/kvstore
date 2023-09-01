29954b2fff1efd0d9b7bbe0e245628e2b6409ab1
mkdemir <mustafakaandemir1@gmail.com>
2023-09-01 10:40:39 +0300

    add server

--- src/internal/storage/memory/kvstorage/set.go
+++ src/internal/storage/memory/kvstorage/set.go
@@ -1,8 +1,19 @@
 package kvstorage
 
-func (ms *memoryStorage) Set(key string, value any) any {
-	ms.mu.Lock()
-	defer ms.mu.Unlock()
+import (
+	"fmt"
+
+	"github.com/mkdemir/kvstore/src/internal/kverror"
+)
+
+func (ms *memoryStorage) Set(key string, value any) (any, error) {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+
+	if _, err := ms.Get(key); err == nil {
+		return nil, fmt.Errorf("%w", kverror.ErrKeyExists.AddData("'"+key+"' already exist"))
+	}
+
 	ms.db[key] = value
-	return value
+	return value, nil
 }
